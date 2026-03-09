a08bf50eee2bfe8eeab1a91ae63c2ae9089f7902
Evgeniya Mikhaylyuk <jmatricaria@yandex.ru>
2026-03-09 22:50:08 +0300

    Добавлены тесты на Cypress для оформления заказа

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,8 +1,11 @@
+const burgerConstructor = '[data-cy="burger-constructor"]';
 const upperBunInConstructor = '[data-cy="constructor-upper-bun"]';
 const mainInConstructor = '[data-cy="constructor-ingredients"]';
 const lowerBunInConstructor = '[data-cy="constructor-lower-bun"]';
 const modalCloseButton = '[data-cy="modal-close"]';
-const modalOverlay = '[data-cy="modal-overlay"]'
+const modalOverlay = '[data-cy="modal-overlay"]';
+const orderButton = '[data-cy="order-button"]';
+const orderNumber = '[data-cy="order-number"]';
 
 const firstBunId = `[data-cy=${'643d69a5c3f7b9001cfa093c'}]`; // Краторная булка N-200i
 const secondBunId = `[data-cy=${'643d69a5c3f7b9001cfa093d'}]`; // Флюоресцентная булка R2-D3
@@ -10,16 +13,18 @@ const mainId = `[data-cy=${'643d69a5c3f7b9001cfa093e'}]`; // Филе Люмин
 const saurceId = `[data-cy=${'643d69a5c3f7b9001cfa0944'}]`; // Соус традиционный галактический
 
 beforeEach(() => {
-  // Перехват запроса на эндпоинт 'api/ingredients’, 
-  // в ответе на который возвращаются моковые данные из ingredients.json
+  // === Перехват запросов === //
   cy.intercept('GET', 'api/ingredients', { fixture: 'ingredients.json' });
-
+  cy.intercept('GET', 'api/auth/user', { fixture: 'user.json' });
+  cy.intercept('POST', 'api/auth/login', { fixture: 'login.json' });
+  cy.intercept('POST', 'api/orders', { fixture: 'post-order.json' }).as('postOrder');
+  
   cy.visit('/');
-  cy.viewport(1680, 1024); //пока так
+  cy.viewport(1920, 1080);
 });
 
 describe('Тестирование на добавление ингредиентов в конcтруктор', function () {
-  
+
   it('Добавление булок', function () {
     cy.get(firstBunId).children('button').click();
     cy.get(upperBunInConstructor).contains('Краторная булка N-200i').should('exist');
@@ -56,7 +61,7 @@ describe('Тестирование модальных окон', function () {
     cy.get('#modals').contains('Филе Люминесцентного тетраодонтимформа').should('exist');
   });
 
-  it('Закрытие модального окна по клику на "крестик"', () => {
+  it('Закрытие модального окна по клику на "крестик"', function () {
     cy.get('#modals').should('be.not.visible');
     cy.get(firstBunId).click();
     cy.get('#modals').should('be.not.empty');
@@ -78,5 +83,46 @@ describe('Тестирование модальных окон', function () {
     cy.get('#modals').should('be.not.empty');
     cy.get('body').trigger('keydown', { key: 'Escape' });
     cy.get('#modals').should('be.not.visible');
+  });
+});
+
+describe('Тестирование создания заказа', function () {
+  this.beforeEach(() => {
+    window.localStorage.setItem('refreshToken',JSON.stringify('testRefreshToken'));
+    cy.getAllLocalStorage().should('be.not.empty');
+
+    cy.setCookie('accessToken', 'testAccessToken');
+    cy.getCookie('accessToken').should('be.not.empty');
+  });
+
+  this.afterEach(() => {
+    cy.clearLocalStorage();
+    cy.clearCookies();
+    cy.getAllLocalStorage().should('be.empty');
+    cy.getAllCookies().should('be.empty');
+  });
+
+  it('Сборка и офросление заказа', function () {
+    // Сборка бургера
+    cy.get(firstBunId).children('button').click();
+    cy.get(mainId).children('button').click();
+    cy.get(saurceId).children('button').click();
+  
+    // Вызывается клик по кнопке «Оформить заказ»
+    cy.get(orderButton).click();
+    cy.wait('@postOrder');
+
+    // Проверяется, что модальное окно открылось и номер заказа верный
+    cy.get('#modals').should('be.not.empty');
+    cy.get(orderNumber).contains('100100').should('exist');
+
+    // Закрывается модальное окно и проверяется успешность закрытия
+    cy.get(modalCloseButton).click();
+    cy.get('#modals').should('be.not.visible');
+
+    // Проверяется, что конструктор пуст
+    cy.get(burgerConstructor).should('not.contain', 'Краторная булка N-200i');
+    cy.get(burgerConstructor).should('not.contain', 'Филе Люминесцентного тетраодонтимформа');
+    cy.get(burgerConstructor).should('not.contain', 'Соус традиционный галактический');
   });
 });
